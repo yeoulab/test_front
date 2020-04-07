202d224b40af7f8e1cc38bbe05c65c0921b9bbc6
yeoulab <myungjin.shin@gmail.com>
2020-04-07 21:50:53 +0000

    modified

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,20 +1,89 @@
 <template>
     <div class="home">
         <v-container>
-            <v-text-field label="item code" outlined v-model="item_code"></v-text-field>
-            <v-text-field label="start date" outlined v-model="start_date"></v-text-field>
-            <v-text-field label="last date" outlined v-model="end_date"></v-text-field>
-            <v-row class="mb-4">
+            <v-row>
                 <v-col>
-                    <v-btn small @click=getInfo>result</v-btn>
+                    <v-text-field
+                        label="item name"
+                        outlined
+                        dense
+                        v-model="item_name"
+                        @change="item_name_change">
+                    </v-text-field>
                 </v-col>
                 <v-col>
-                    <v-btn small @click=getCompInfo>기업정보</v-btn>
+                    <v-text-field 
+                        label="item code" 
+                        outlined 
+                        dense
+                        v-model="item_code"
+                        @change="item_change">
+                    </v-text-field>
                 </v-col>
             </v-row>
+            <v-row>
+                <v-col>
+                    <v-text-field 
+                        label="start date" 
+                        outlined 
+                        dense
+                        v-model="start_date">
+                    </v-text-field>
+                </v-col>
+                <v-col>
+                    <v-text-field 
+                        label="last date" 
+                        outlined 
+                        dense
+                        v-model="end_date">
+                    </v-text-field>
+                </v-col>
+            </v-row>            
+            <v-row class="mb-4">
+                <v-col>
+                    <v-btn large @click=getInfo>Result from api</v-btn>
+                </v-col>
+                <v-col>
+                    <v-btn large @click=getCompInfo>Company Info</v-btn>
+                </v-col>
+            </v-row>
+            <div v-if="code_search_yn">
+                <v-data-table
+                    dense
+                    :headers="headers"
+                    :items="code_info"
+                    hide-default-footer
+                    item-key="subject">
+                </v-data-table>
+                <!--
+                <v-simple-table dense>
+                    <template v-slot:default>
+                        <thead>
+                            <tr>
+                                <th class="text-center">Subject</th>
+                                <th class="text-center">Value</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr v-for="item in code_info" :key="item.subject">
+                            <td>{{ item.subject }}</td>
+                            <td>{{ item.value }}</td>
+                            </tr>
+                        </tbody>
+                    </template>
+                </v-simple-table>-->
+            </div>            
             <br>
             <div v-if="search_yn">
-                <v-simple-table>
+                <v-data-table
+                    dense
+                    :headers="headers2"
+                    :items="data.datas"
+                    hide-default-footer
+                    item-key="subject">
+                </v-data-table>
+                <!--
+                <v-simple-table dense>
                     <template v-slot:default>
                     <thead>
                         <tr>
@@ -31,7 +100,8 @@
                         </tr>
                     </tbody>
                     </template>
-                </v-simple-table>            
+                </v-simple-table>
+                -->
             </div>
         </v-container>
     </div>
@@ -41,22 +111,35 @@ import axios from 'axios'
 
 export default{
     data() {
-        return{
+        return{           
+            item_name: '', 
+            item_code: '',
+            start_date: '',
+            end_date: '',
+            baseUrl:  "http://34.64.244.176:8090",
+            baseLink: 'https://navercomp.wisereport.co.kr/v2/company/c1010001.aspx?cmp_cd=',
+            link: '',
+            headers: [
+                { text: 'Subject', value: 'subject'},
+                { text: 'Value', value: 'value'}
+            ],
+            code_search_yn: false,
+            code_info: [],
+            headers2: [
+                { text: 'Subject', value: 'subject'},
+                { text: 'Value', value: 'value'},
+                { text: 'preValue', value: 'pre_value' }
+            ],
+            search_yn: false,
             data: {
                 datas: [],
                 pre_datas: [],
             },
-            item_code: '',
-            start_date: '',
-            end_date: '',
-            link: 'https://navercomp.wisereport.co.kr/v2/company/c1010001.aspx?cmp_cd=',
-            search_yn: false,
         }
     },
     methods: {
         getInfo(){
-            const baseUrl = "http://127.0.0.1:5000"
-            axios.get(`${baseUrl}/unitPrice`,{
+            axios.get(`${this.baseUrl}/unitPrice`,{
                 params: {
                     item_code: this.item_code,
                     start_date: this.start_date,
@@ -77,11 +160,44 @@ export default{
 
                 console.log(this.data.datas[0])                
 
-                this.link = this.link + this.item_code
             })
         },
         getCompInfo(){
             window.open(this.link ,"");
+        },
+        item_name_change(){
+            axios.get(`${this.baseUrl}/code`,{
+                params: {
+                    item_name: this.item_name,
+                }
+            })
+            .then((result) =>{
+                console.log(result)
+                this.item_code = result.data
+            })
+        },
+        item_change(){
+            if( this.item_code.length == 6 ){
+                // item 정보 조회하는 api 호출
+                axios.get(`${this.baseUrl}/codeInfo`,{
+                    params: {
+                        item_code: this.item_code,
+                    }
+                })
+                .then((result) =>{
+                    console.log(result)
+                    this.code_info = result.data
+                    this.code_search_yn = true
+                    this.link = this.baseLink + this.item_code
+                })
+            }
+            else{
+                this.link = this.baseLink
+                this.code_search_yn = false
+                this.search_yn = false
+                this.data.datas = []
+                this.code_info = []
+            }
         }
     },
     mounted() {
@@ -102,4 +218,5 @@ export default{
     padding-bottom: 5px;
 }
 </style>
+
 
